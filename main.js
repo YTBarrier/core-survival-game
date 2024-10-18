cd9be5d5ac7a4651b1fe4f205aed2c46a71fc62a
YTBarrier <144122348+YTBarrier@users.noreply.github.com>
2024-10-18 20:00:58 +0000

    Removed Wix files

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { PointerLockControls } from 'three/examples/jsm/Addons.js';
 import * as World from './modules/world.js';
-import * as Utils from './modules/utils.js'
+import * as Utils from './modules/utils.js';
 
 // scene, camera, and renderer setup
 const scene = new THREE.Scene();
@@ -24,38 +24,8 @@ document.addEventListener('click', () => {
 
 // movement controls
 let movement = { forward: false, backward: false, left: false, right: false };
-document.addEventListener('keydown', e => {
-    switch(e.code) {
-        case 'KeyW':
-            movement.forward = true;
-            break;
-        case 'KeyS':
-            movement.backward = true;
-            break;
-        case 'KeyA':
-            movement.left = true;
-            break;
-        case 'KeyD':
-            movement.right = true;
-            break;
-    }
-});
-document.addEventListener('keyup', e => {
-    switch(e.code) {
-        case 'KeyW':
-            movement.forward = false;
-            break;
-        case 'KeyS':
-            movement.backward = false;
-            break;
-        case 'KeyA':
-            movement.left = false;
-            break;
-        case 'KeyD':
-            movement.right = false;
-            break;
-    }
-});
+Utils.initializeMovement(movement);
+
 
 // objects to render
 scene.add(World.generateWorld());
@@ -68,36 +38,19 @@ let coords = {
     x: 0,
     y: 0,
     z: 0,
+    update: function() {
+        coords.x = Math.round(camera.position.x);
+        document.getElementById('x').innerHTML = `X: ${coords.x}`;
+        coords.y = Math.round(camera.position.y - 6);
+        document.getElementById('y').innerHTML = `Y: ${coords.y}`;
+        coords.z = Math.round(camera.position.z);
+        document.getElementById('z').innerHTML = `Z: ${coords.z}`;
+    },
 }
 renderer.setAnimationLoop(function() {
-    if(movement.forward) {
-        const movementDistance = 1;
-        const direction = new THREE.Vector3();
-        camera.getWorldDirection(direction);
-        const horizontalDirection = direction.clone().setY(0).normalize();
+    Utils.movementCheck(movement, camera);
 
-        camera.position.addScaledVector(horizontalDirection, movementDistance);
-    }
-    if(movement.backward) {
-        const movementDistance = -1;
-        const direction = new THREE.Vector3();
-        camera.getWorldDirection(direction);
-        const horizontalDirection = direction.clone().setY(0).normalize();
-        
-        camera.position.addScaledVector(horizontalDirection, movementDistance);
-    }
-    if(movement.left) {
-        camera.translateX(-1);
-    }
-    if(movement.right) {
-        camera.translateX(1);
-    }
-    coords.x = Math.round(camera.position.x);
-    document.getElementById('x').innerHTML = `X: ${coords.x}`;
-    coords.y = Math.round(camera.position.y - 6);
-    document.getElementById('y').innerHTML = `Y: ${coords.y}`;
-    coords.z = Math.round(camera.position.z);
-    document.getElementById('z').innerHTML = `Z: ${coords.z}`;
+    coords.update();
 
     renderer.render(scene, camera);
 });
--- modules/utils.js
+++ modules/utils.js
@@ -1,8 +1,64 @@
 import * as THREE from 'three';
-function initializeWeatherCycle(clearColor) {
-    setInterval(execWeatherCycle, 5);
-    function execWeatherCycle() {
+function execWeatherCycle(clearColor) {
 
+}
+
+function initializeMovement(movement) {
+    document.addEventListener('keydown', e => {
+        switch(e.code) {
+            case 'KeyW':
+                movement.forward = true;
+                break;
+            case 'KeyS':
+                movement.backward = true;
+                break;
+            case 'KeyA':
+                movement.left = true;
+                break;
+            case 'KeyD':
+                movement.right = true;
+                break;
+        }
+    });
+    document.addEventListener('keyup', e => {
+        switch(e.code) {
+            case 'KeyW':
+                movement.forward = false;
+                break;
+            case 'KeyS':
+                movement.backward = false;
+                break;
+            case 'KeyA':
+                movement.left = false;
+                break;
+            case 'KeyD':
+                movement.right = false;
+                break;
+        }
+    });
+}
+function movementCheck(movement, camera) {
+    if(movement.forward) {
+        const movementDistance = 0.1;
+        const direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+        const horizontalDirection = direction.clone().setY(0).normalize();
+
+        camera.position.addScaledVector(horizontalDirection, movementDistance);
+    }
+    if(movement.backward) {
+        const movementDistance = -0.1;
+        const direction = new THREE.Vector3();
+        camera.getWorldDirection(direction);
+        const horizontalDirection = direction.clone().setY(0).normalize();
+        
+        camera.position.addScaledVector(horizontalDirection, movementDistance);
+    }
+    if(movement.left) {
+        camera.translateX(-0.1);
+    }
+    if(movement.right) {
+        camera.translateX(0.1);
     }
 }
-export { initializeWeatherCycle };
+export { execWeatherCycle, initializeMovement, movementCheck };
